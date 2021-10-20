57813ceaa4584c3ec8f7070f510f3822a54a2281
moofMonkey <1892802-moofMonkey@users.noreply.gitlab.com>
2021-10-20 09:32:51 +0000

    Initial commit

---
+++ main.ts
@@ -0,0 +1,4 @@
+import { RendererSDK } from "./wrapper/Imports"
+
+RendererSDK.CreateFont("Noto Sans Japanese", "fonts/NotoSansJP/NotoSansJP-Bold.otf", 700, false)
+RendererSDK.CreateFont("Noto Sans Japanese", "fonts/NotoSansJP/NotoSansJP-Regular.otf", 400, false)
