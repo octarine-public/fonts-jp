21c9848ab7fe7850e0334746a4a4493124a8aa27
SicdeX <sicdex@gmail.com>
2022-09-05 13:51:09 +0300

    fixed imports

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-import { RendererSDK } from "./wrapper/Imports"
+import { RendererSDK } from "github.com/octarine-public/wrapper/wrapper/Imports"
 
 RendererSDK.CreateFont("Noto Sans Japanese", "fonts/NotoSansJP/NotoSansJP-Bold.otf", 700, false)
 RendererSDK.CreateFont("Noto Sans Japanese", "fonts/NotoSansJP/NotoSansJP-Regular.otf", 400, false)
