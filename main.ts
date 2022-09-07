0160016e57aee76abfa0061bfad38c7c0389d3a0
MoofMonkey <11695747+MoofMonkey@users.noreply.github.com>
2022-09-07 21:29:44 +0300

    Update main.ts

--- main.ts
+++ main.ts
@@ -8,8 +8,5 @@ function LoadFontWrapper(path: string, isFallback = false) {
 		console.error(`Failed to find font file ${path}`)
 }
 
-RendererSDK.CreateFont("Noto Sans Japanese", "fonts/NotoSansJP/NotoSansJP-Bold.otf", 700, false)
-RendererSDK.CreateFont("Noto Sans Japanese", "fonts/NotoSansJP/NotoSansJP-Regular.otf", 400, false)
-
 LoadFontWrapper("fonts/NotoSansJP/NotoSansJP-Bold.otf")
 LoadFontWrapper("fonts/NotoSansJP/NotoSansJP-Regular.otf", true)
