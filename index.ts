1bea4e758c9fbc1fa40d28fb730952abb6c6c745
SicdeX <sicdex@gmail.com>
2022-12-01 12:54:26 +0600

    lint

--- index.ts
+++ index.ts
@@ -2,8 +2,11 @@ import { tryFindFile } from "github.com/octarine-public/wrapper/index"
 
 function LoadFontWrapper(path: string, isFallback = false) {
 	const realPath = tryFindFile(path)
-	if (realPath !== undefined) LoadFont(realPath, isFallback)
-	else console.error(`Failed to find font file ${path}`)
+	if (realPath !== undefined) {
+		LoadFont(realPath, isFallback)
+	} else {
+		console.error(`Failed to find font file ${path}`)
+	}
 }
 
 LoadFontWrapper("fonts/MPLUS1/MPLUS1-VariableFont_wght.ttf", true)
